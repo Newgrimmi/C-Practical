0e30e81888ea2b38ff9cf13d2af388c2d119b6e4
Newgrimmi <newgrimmi2@mail.ru>
2022-11-18 16:11:58 +0300

    Create Program.cs

---
+++ 1PracticalC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1PracticalC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Hello World!!!");
+            Console.Write("Hello");
+            Console.Write(" World");
+            Console.Write(" !!! ");
+            Console.WriteLine("Second Hello World!!!");
+            Console.ReadKey();
+        }
+    }
+}
