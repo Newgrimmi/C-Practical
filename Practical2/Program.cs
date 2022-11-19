bf3268a8e6bdd4fb8492edd719553a3c6bc43a69
Newgrimmi <newgrimmi2@mail.ru>
2022-11-19 22:52:11 +0300

    Create Program.cs

---
+++ Practical2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region Exercise 1
+
+            //string fullName = "Быков Станислав Викторович";
+            //int age = 24;
+            //string eMail = "newgrimmi2@mail.ru";
+            //float informaticsScore = 95f;
+            //float mathScore = 54f;
+            //float physicsScore = 66f;
+
+            //string newPattern = "Ф.И.О.: {0} \nВозраст: {1} \nEmail: {2} \nБаллы по информатике: {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
+
+            //Console.WriteLine(newPattern,
+            //                   fullName,
+            //                   age,
+            //                   eMail,
+            //                   informaticsScore,
+            //                   mathScore,
+            //                   physicsScore);
+            //Console.ReadKey();
+            #endregion
+
+            #region Exercise 2
+
+            //float informaticsScore = 95f;
+            //float mathScore = 54f;
+            //float physicsScore = 66f;
+            //float sum;
+            //string averageScore; // без округления float averageScore
+
+            //sum = informaticsScore + mathScore + physicsScore;
+            //averageScore = (sum / 3).ToString("0.00"); //Чтобы округлить, если убрать то в 42 строке
+            //string newPattern = "Сумма баллов: {0} \nСреднеарифметическое: {1}";
+            //Console.WriteLine(newPattern,
+            //                   sum,
+            //                   averageScore);
+            //Console.ReadKey();
+
+
+            #endregion
+            // float экономит место по сравнению с double и такая точность не нужно вроде...
+        }
+    }
+}
