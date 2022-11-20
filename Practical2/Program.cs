58ac7a45da72f63416ddd70f50bef90f8a0783ba
Newgrimmi <newgrimmi2@mail.ru>
2022-11-20 18:07:41 +0300

    Update Program.cs

--- Practical2/Program.cs
+++ Practical2/Program.cs
@@ -10,47 +10,33 @@ namespace Practical2
     {
         static void Main(string[] args)
         {
+            string fullName = "Быков Станислав Викторович";
+            int age = 24;
+            string eMail = "newgrimmi2@mail.ru";
+            float informaticsScore = 95f;
+            float mathScore = 54f;
+            float physicsScore = 66f;
 
+            string newPattern = "Ф.И.О.: {0} \nВозраст: {1} \nEmail: {2} \nБаллы по информатике: {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
 
-            #region Exercise 1
+            Console.WriteLine(newPattern,
+                               fullName,
+                               age,
+                               eMail,
+                               informaticsScore,
+                               mathScore,
+                               physicsScore);
+            float sum;
+            string averageScore; // без округления float averageScore
 
-            //string fullName = "Быков Станислав Викторович";
-            //int age = 24;
-            //string eMail = "newgrimmi2@mail.ru";
-            //float informaticsScore = 95f;
-            //float mathScore = 54f;
-            //float physicsScore = 66f;
+            sum = informaticsScore + mathScore + physicsScore;
+            averageScore = (sum / 3).ToString("0.00"); //Чтобы округлить, если убрать то в 42 строке
+            string newPattern2 = "Сумма баллов: {0} \nСреднеарифметическое: {1}";
+            Console.WriteLine(newPattern2,
+                               sum,
+                               averageScore);
+            Console.ReadKey();
 
-            //string newPattern = "Ф.И.О.: {0} \nВозраст: {1} \nEmail: {2} \nБаллы по информатике: {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
-
-            //Console.WriteLine(newPattern,
-            //                   fullName,
-            //                   age,
-            //                   eMail,
-            //                   informaticsScore,
-            //                   mathScore,
-            //                   physicsScore);
-            //Console.ReadKey();
-            #endregion
-
-            #region Exercise 2
-
-            //float informaticsScore = 95f;
-            //float mathScore = 54f;
-            //float physicsScore = 66f;
-            //float sum;
-            //string averageScore; // без округления float averageScore
-
-            //sum = informaticsScore + mathScore + physicsScore;
-            //averageScore = (sum / 3).ToString("0.00"); //Чтобы округлить, если убрать то в 42 строке
-            //string newPattern = "Сумма баллов: {0} \nСреднеарифметическое: {1}";
-            //Console.WriteLine(newPattern,
-            //                   sum,
-            //                   averageScore);
-            //Console.ReadKey();
-
-
-            #endregion
             // float экономит место по сравнению с double и такая точность не нужно вроде...
         }
     }
