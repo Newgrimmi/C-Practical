c1e82158268d0c15161ad3afbb43157dc73db965
Newgrimmi <newgrimmi2@mail.ru>
2022-11-24 22:55:16 +0300

    5Practical

---
+++ Practical5/Task1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Напишите предложение");
+            string sentence = Console.ReadLine();
+            string[] spltText = SplitText(sentence);
+            WriteWords(spltText);
+
+            //WriteWords(SplitText(sentence));
+        }
+
+        public static int CountWords(char[] let)
+        {
+            int countWord = 1;
+            char[] letters = new char[let.Length];
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letters[i] = let[i];
+                if (letters[i] == ' ')
+                {
+                    countWord++;
+                }
+            }
+            return countWord;
+        }
+
+        public static string[] SplitText(string text)
+        {
+            char[] letters = text.ToCharArray();
+            int countWords = CountWords(letters);
+            string[] words = new string[countWords];
+            int numberWord = 0;
+            string word = "";
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (i == letters.Length - 1)
+                {
+                    word += letters[i];
+                    words[numberWord] = word;
+                }
+                else if (letters[i] != ' ')
+                {
+                    word += letters[i];
+                }
+                else
+                {
+                    words[numberWord] = word;
+                    numberWord++;
+                    word = "";
+                }
+            }
+
+            return words;
+        }
+
+        public static void WriteWords(string[] words)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine($"\n{words[i]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practical5/Task2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Напишите предложение");
+            string sentence = Console.ReadLine();
+
+            Reverse(SplitText(sentence));
+        }
+
+        public static int CountWords(char[] let)
+        {
+            int countWord = 1;
+            char[] letters = new char[let.Length];
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letters[i] = let[i];
+
+                if (letters[i] == ' ')
+                {
+                    countWord++;
+                }
+            }
+            return countWord;
+        }
+
+        public static string[] SplitText(string text)
+        {
+            char[] letters = text.ToCharArray();
+            int countWords = CountWords(letters);
+            string[] words = new string[countWords];
+            int numberWord = 0;
+            string word = "";
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+
+                if (i == letters.Length - 1)
+                {
+                    word += letters[i];
+                    words[numberWord] = word;
+                }
+                else if (letters[i] != ' ')
+                {
+                    word += letters[i];
+                }
+                else
+                {
+                    words[numberWord] = word;
+                    numberWord++;
+                    word = "";
+                }
+            }
+
+            return words;
+        }
+
+        public static string Reverse(string[] text)
+        {
+            int firstWord = 0;
+            int lastWord = text.Length-1;
+            string average;
+            string reverseText = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (firstWord < lastWord)
+                {
+                    average = text[firstWord];
+                    text[firstWord] = text[lastWord];
+                    text[lastWord] = average;
+                }
+                firstWord++;
+                lastWord--;
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                reverseText += text[i] + " ";
+            }
+            Console.WriteLine($"{reverseText}");
+            Console.ReadKey();
+
+            return reverseText;
+        }
+    }
+}
