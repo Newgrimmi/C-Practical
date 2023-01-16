87ba51bb3ce0400defbd367eeb669676ad3b3bcd
Newgrimmi <118377894+Newgrimmi@users.noreply.github.com>
2023-01-16 19:07:06 +0300

    Update Program.cs

--- Practical6/Task1/Program.cs
+++ Practical6/Task1/Program.cs
@@ -1,113 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.IO;
-using System.Linq;
+using System.Text;
 
 namespace Task1
 {
     class Program
     {
-        static ListNode Head = new ListNode();
-        static ListNode Tail = new ListNode();
-        static int Count;
-
         static void Main()
         {
-            string StartDirectory = @"c:\Test\Test1";
-            var stream = new FileStream(StartDirectory, FileMode.Create);
-            Deserialize(stream);
-            Console.WriteLine("List serialized");
-            Console.ReadKey();
+            MainActive(); 
+        }
+
+        static void MainActive()
+        {
+            bool menuActive = true;
+            while (menuActive)
+            {
+                Console.WriteLine("Введите 1, если хотите просмотреть данные");
+                Console.WriteLine("Введите 2, если хотите внести данные");
+                Console.WriteLine("Введите 3, если хотите выйти");
+                string curNumb = Console.ReadLine();
+                switch (curNumb)
+                {
+                    case "1":
+                        ReadEmployeeInfo();
+                        break;
+                    case "2":
+                        WriteEmployee();
+                        break;
+                    case "3":
+                        menuActive = false;
+                        break;
+                    default:
+                        Console.WriteLine("Введено не верное значение");
+                        break;
+                }
+            }
+        }
+
+        static void WriteEmployee()
+        {
+            StringBuilder employeeInfo = new StringBuilder();
+
+            if (File.Exists("employeeData.txt") == false)
+            {
+                employeeInfo.Append("1");
+            }
+            else
+            {
+                string[] employeeCount = File.ReadAllLines("employeeData.txt");
+                employeeInfo.Append("\n" + (employeeCount.Length + 1));
+            }
+
+            Console.WriteLine("Введите дату и время добавления записи: дд.мм.гггг чч:мм");
+            employeeInfo.Append(" " + Console.ReadLine());
+            Console.WriteLine("Введите Ф.И.О.");
+            employeeInfo.Append(" " + Console.ReadLine());
+            Console.WriteLine("Введите возраст");
+            employeeInfo.Append(" " + Console.ReadLine());
+            Console.WriteLine("Рост");
+            employeeInfo.Append(" " + Console.ReadLine());
+            Console.WriteLine("Дату рождения: дд.мм.гггг");
+            employeeInfo.Append(" " + Console.ReadLine());
+            Console.WriteLine("Введите место рождения");
+            employeeInfo.Append(" " + "город:" + Console.ReadLine());
+
+            File.AppendAllText("employeeData.txt", employeeInfo.ToString());
+        }
+
+        static void ReadEmployeeInfo()
+        {
+            if (File.Exists("employeeData.txt") == false)
+            {
+                WriteEmployee();
+            }
+            else
+            {
+                string[] info = File.ReadAllLines("employeeData.txt");
+                for (int i = 0; i < info.Length; i++)
+                {
+                    Console.WriteLine($"{info[i]}");
+                }
+                Console.ReadKey();
+            }
             
-        }
-
-        
-
-        static ListNode GetNodeAt(int index)
-        {
-            int counter = 0;
-            for (ListNode currentNode = Head; currentNode.Next != null; currentNode = currentNode.Next)
-            {
-                if (counter == index)
-                    return currentNode;
-                counter++;
-            }
-            return new ListNode();
-        }
-
-
-        static void Serialize(Stream s)
-        {
-            Dictionary<ListNode, int> dictionary = new Dictionary<ListNode, int>();
-            int id = 0;
-            for (ListNode currentNode = Head; currentNode != null; currentNode = currentNode.Next)
-            {
-                dictionary.Add(currentNode, id);
-                id++;
-            }
-            using (BinaryWriter writer = new BinaryWriter(s))
-            {
-                for (ListNode currentNode = Head; currentNode != null; currentNode = currentNode.Next)
-                {
-                    writer.Write(currentNode.Data);
-                    writer.Write(dictionary[currentNode.Random]);
-                }
-            }
-            Console.WriteLine("List serialized");
-
-        }
-
-        static void Deserialize(Stream s)
-        {
-            Dictionary<int, Tuple<String, int>> dictionary = new Dictionary<int, Tuple<String, int>>();
-            int counter = 0;
-            using (BinaryReader reader = new BinaryReader(s))
-            {
-                while (reader.PeekChar() != -1)
-                {
-                    String data = reader.ReadString();
-                    int randomId = reader.ReadInt32();
-                    dictionary.Add(counter, new Tuple<String, int>(data, randomId));
-                    counter++;
-                }
-                Console.WriteLine("File readed");
-            }
-            Count = counter;
-            Head = new ListNode();
-            ListNode current = Head;
-            for (int i = 0; i < Count; i++)
-            {
-                current.Data = dictionary.ElementAt(i).Value.Item1;
-                current.Next = new ListNode();
-                if (i != Count - 1)
-                {
-                    current.Next.Previous = current;
-                    current = current.Next;
-                }
-                else
-                {
-                    Tail = current;
-                }
-            }
-            counter = 0;
-            for (ListNode currentNode = Head; currentNode.Next != null; currentNode = currentNode.Next)
-            {
-                currentNode.Random = GetNodeAt(dictionary.ElementAt(counter).Value.Item2);
-                counter++;
-            }
-            Console.WriteLine("List deserialized");
         }
     }
 }
-
-        public class ListNode
-        {
-            public ListNode Previous;
-            public ListNode Next;
-            public ListNode Random;
-            public string Data = "1";
-        }
-
-        
-
-      
